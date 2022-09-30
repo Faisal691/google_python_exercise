14b8645aba3be12184f916f63472b8bbd1d0fd78
Faisal Khan <fabkohl1@gmail.com>
2022-09-30 19:23:15 +0530

    list2 ka 2

--- basic/list2.py
+++ basic/list2.py
@@ -30,7 +30,12 @@ def remove_adjacent(nums):
 # pass of both lists.
 def linear_merge(list1, list2):
   # +++your code here+++
-  return
+  res=[]
+  res=sorted(list1 + list2)
+  print(res)
+  return res
+
+
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
